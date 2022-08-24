6c22b523b95480217da94353398484001e2cb84d
ulags-world <world.j.github@gmail.com>
2022-08-24 12:59:51 +0530

    Final

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector('.menu');
 const head2 = document.querySelector('.head-2');
 const head = document.querySelector('.head');
 const belowhead = document.querySelector('below-head');
+console.log(belowhead);
 navBtn.addEventListener('click',()=>{
   head2.classList.toggle('active');
   head.classList.toggle('active');
@@ -12,4 +13,5 @@ navBtn.addEventListener('click',()=>{
   if(icon.classList.contains('bx-menu')) icon.classList.replace('bx-menu','bx-x');
   else icon.classList.replace('bx-x','bx-menu');
   menu.classList.toggle('toggle');
+  
 })
